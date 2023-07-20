7a8e247fa2094344b5c69160022ca70b40d1092a
vu giap <vudinhgiap11@gmail.com>
2023-07-20 10:28:55 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();
+            Student stu1 = new Student(1, "ngoc");
+            Student stu2 = new Student(2, "giang");
+            Student stu3 = new Student(3, "huyen");
+
+            stu1.Showinfo();
+            stu2.Showinfo();
+            stu3.Showinfo();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticMethod
+{
+    internal class Student
+    {
+        private int msv;
+        private string name;
+        private static string college = "abc";
+
+        public Student(int msv, string name)
+        {
+            this.msv = msv;
+            this.name = name;
+        }
+
+        public int Msv { get => msv; set => msv = value; }
+        public string Nam { get => name; set => name = value; }
+
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+
+        public void Showinfo()
+        {
+            Console.WriteLine(msv + name + " " + college);
+        }
+    }
+}
